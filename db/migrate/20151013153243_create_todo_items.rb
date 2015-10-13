b856b8273996ca0821c2039036a479d00bce25cc
paolabaccaro <paolabaccaro@hotmail.com>
2015-10-13 11:34:19 -0400

    first rails project

---
+++ db/migrate/20151013153243_create_todo_items.rb
@@ -0,0 +1,11 @@
+class CreateTodoItems < ActiveRecord::Migration
+  def change
+    create_table :todo_items do |t|
+      t.string :description
+      t.string :—javascript­-engine=js
+      t.string :­--stylesheet-­engine=css
+
+      t.timestamps null: false
+    end
+  end
+end
